14acc351b3d836e76d0918f03c02fa84ef307290
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-10-19 14:15:15 +0800

    backend-api:added auth and habit routes with middleware

--- routes/habit.routes.ts
+++ routes/habit.routes.ts
@@ -4,7 +4,7 @@ import { auth_check } from "../middlewares/authentication";
 
 const habitRouter: IRouter = Router();
 
-habitRouter.get("/", getUserHabits);
+habitRouter.get("/", auth_check, getUserHabits);
 habitRouter.post("/new", auth_check, createHabit);
 habitRouter.put("/update", auth_check, updateHabit);
 habitRouter.delete("/delete", auth_check, deleteHabit);
