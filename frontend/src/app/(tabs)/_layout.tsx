80330e0864fde0dec11877ebe729a551abd7579f
Alex Abainza <abainza.alexandra@gmail.com>
2024-11-08 12:42:17 +0800

    frontend: set up 5 main tabs and path aliasing

---
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Colors from "@constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof Entypo>["name"];
+  color: string;
+}) {
+  return <Entypo size={32} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarInactiveTintColor: Colors[colorScheme ?? "light"].gray,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].text,
+        headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: "#ffffff",
+          paddingBottom: 5,
+          height: 65,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="habits"
+        options={{
+          title: "Habits",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="ruler" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="circle-with-plus" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="statistics"
+        options={{
+          title: "Statistics",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="bar-graph" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
