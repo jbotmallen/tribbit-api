0499495d12e8e7fad1b3354779e118c377067c1a
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-11-28 00:01:18 +0800

    refactor:auth pages changed layout and color

--- models/habit.models.ts
+++ models/habit.models.ts
@@ -3,6 +3,7 @@ import { model, Schema, models, Model, Types } from "mongoose";
 type HabitDocument = Document & {
     name: string;
     goal: number;
+    colorScheme: string;
     user_id: string;
 }
 
@@ -10,6 +11,7 @@ const habitSchema = new Schema({
     name: { type: String, required: true },
     goal: { type: Number, required: true },
     user_id: { type: Types.ObjectId, ref: "User", required: true },
+    colorScheme: { type: String, default: "#000000" },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
     deleted_at: { type: Date, default: null },
